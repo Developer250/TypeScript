bb42228befaa715ef80ebaf767c89453221c28fb
Developer250 <jani.kekalainen@hotmail.com>
2023-09-06 00:03:56 +0300

    JAtka

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="login">
             <div className="loginbox">
-                <div className="loginheader"></div>
+                <div className="loginheader">Login</div>
                 <div className="inputs">
                     <input className="email" placeholder="enter your email"></input>
                     <input className="password" placeholder="enter your password" type="password" ></input>

17d0db5db82d89ef5ff18de2d70497c677465411
Developer250 <jani.kekalainen@hotmail.com>
2023-09-06 20:12:45 +0300

    Tee inputtien validiointi ja submit napin toimivuus

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import { send } from 'process';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+    const [inputs, setInputs] = useState({ email: '', password: '' });
+
+   
+
+
     return (
         <div className="login">
             <div className="loginbox">
                 <div className="loginheader">Login</div>
                 <div className="inputs">
-                    <input className="email" placeholder="enter your email"></input>
-                    <input className="password" placeholder="enter your password" type="password" ></input>
+                    <input className="email" type="text" placeholder="enter your email"></input>
+                    <input className="password" placeholder="enter your password" type="password" value={inputs.password || ""}
+                        ></input>
                 </div>
                 <div>
-                    <button className="btn">login</button>
+
+                    <button className="btn" type="submit">Login</button>
                 </div>
             </div>
-            </div>
-        
-  );
+        </div>
+
+    );
 }
+    
+
 
 export default App;

2e75bef7fb8fd624ca8c7840f63b57eda399143c
Developer250 <jani.kekalainen@hotmail.com>
2023-09-06 23:07:04 +0300

    Submit button toimii. Tee inputtien validioinnit

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,31 @@ import { send } from 'process';
 import React, { useState } from 'react';
 import './App.css';
 
-
 function App() {
-    const [inputs, setInputs] = useState({ email: '', password: '' });
 
-   
+    const submit = () => {
 
+        alert("Your form has been sent succesfully!");
+    }
 
     return (
         <div className="login">
             <div className="loginbox">
                 <div className="loginheader">Login</div>
                 <div className="inputs">
-                    <input className="email" type="text" placeholder="enter your email"></input>
-                    <input className="password" placeholder="enter your password" type="password" value={inputs.password || ""}
+                    <input className="username"
+                        type="text" placeholder="enter your username">
+                    </input>
+                    <input className="password"
+                        placeholder="enter your password"
+                        type="password"
                         ></input>
                 </div>
                 <div>
-
-                    <button className="btn" type="submit">Login</button>
+                    <button onClick={submit} className="btn" type="submit">Login</button>
                 </div>
             </div>
         </div>
-
     );
 }
-    
-
-
 export default App;
